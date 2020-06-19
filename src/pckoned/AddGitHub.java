2cb78f24df9eec129edca22da3da61d31c8bf2d3
Sravan <sravan.mk19@gmail.com>
2020-06-19 23:17:10 +0530

    CR10

---
+++ src/pckoned/AddGitHub.java
@@ -0,0 +1,15 @@
+package pckoned;
+
+public class AddGitHub {
+
+	public static void main(String[] args) 
+	{
+		int a=10; 
+		int b=30;
+		int c=a+b;
+		System.out.println("the value of C is: "+ c);
+
+
+	}
+
+}
